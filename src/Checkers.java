b32f4b9482a2e8c9b98303cd04209070d0b0d0b9
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-04-12 16:25:44 +0200

    can get legal moves and setup board

--- src/Checkers.java
+++ src/Checkers.java
@@ -1,29 +1,135 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
+// board is index 1-32
 
 public class Checkers {
+    private static final int EMPTY = 0;
+    private static final int OUT_OF_BOUNDS = 3;
+    private static final int PLAYER_ONE_PAWN = 1;
+    private static final int PLAYER_TWO_PAWN = -1;
     private int[] board;
 
-    public Checkers(int[] board) {
-        this.board = board;
+    public Checkers(int[] b) {
+        board = b;
     }
 
-    public Checkers() { }
+    public Checkers() {
+    }
 
     public void setup() {
-        this.board = new int[]{1,1,1,1, 1,1,1,1, 1,1,1,1, 0,0,0,0, 0,0,0,0, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1};
+        board = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     }
 
     public String toString() {
-        return Arrays.toString(this.board);
+        return Arrays.toString(board);
     }
 
     public List<Move> getLegalMoves(int i) {
-        ArrayList<Move> moves = new ArrayList<Move>() {};
-        if (this.board[i + 4] == 0) {
-            moves.add(new Move(i, i + 4));
+        ArrayList<Move> moves = new ArrayList<Move>() {
+        };
+
+        if (this.getSquare(i) > 0) {
+            Move leftDown = leftDownMove(i);
+            if (leftDown != null) {
+                moves.add(leftDown);
+            }
+
+            Move rightDown = rightDownMove(i);
+            if (rightDown != null) {
+                moves.add(rightDown);
+            }
         }
+
+        if (this.getSquare(i) < 0) {
+            Move leftUp = leftUpMove(i);
+            if (leftUp != null) {
+                moves.add(leftUp);
+            }
+
+            Move rightUp = rightUpMove(i);
+            if (rightUp != null) {
+                moves.add(rightUp);
+            }
+        }
+
         return moves;
+    }
+
+    public List<Move> getAllLegalMoves(int player) {
+        List<Integer> playerPawnSquares = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            if (getSquare(i) == pawnValue(player)) {
+                playerPawnSquares.add(i);
+            }
+        }
+
+        return playerPawnSquares.stream()
+                .map(this::getLegalMoves)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    private int pawnValue(int player) {
+        return player == 1 ? PLAYER_ONE_PAWN : PLAYER_TWO_PAWN;
+    }
+
+    private Move leftUpMove(int i) {
+        int leftIndex = isEven(row(i)) ? i - 4 : i - 5;
+
+        if (row(i) - 1 == row(leftIndex) && getSquare(leftIndex) == EMPTY) {
+            return new Move(i, leftIndex);
+        }
+        return null;
+    }
+
+    private Move rightUpMove(int i) {
+        int rightIndex = isEven(row(i)) ? i - 5 : i - 4;
+
+        if (row(i) - 1 == row(rightIndex) && getSquare(rightIndex) == EMPTY) {
+            return new Move(i, rightIndex);
+        }
+        return null;
+    }
+
+    private Move leftDownMove(int i) {
+        int leftIndex = isEven(row(i)) ? i + 4 : i + 3;
+
+        if (row(i) + 1 == row(leftIndex) && getSquare(leftIndex) == EMPTY) {
+            return new Move(i, leftIndex);
+        }
+        return null;
+    }
+
+    private Move rightDownMove(int i) {
+        int rightIndex = isEven(row(i)) ? i + 5 : i + 4;
+
+        if (row(i) + 1 == row(rightIndex) && getSquare(rightIndex) == EMPTY) {
+            return new Move(i, rightIndex);
+        }
+        return null;
+    }
+
+    private boolean isEven(int row) {
+        return row % 2 == 0;
+    }
+
+    private int getSquare(int i) {
+        int boardIndex = i - 1;
+        if (boardIndex < 1 || boardIndex > board.length) {
+            return OUT_OF_BOUNDS;
+        } else {
+            return board[boardIndex];
+        }
+    }
+
+    ;
+
+    private int row(int i) {
+        return Math.floorDiv(i - 1, 4);
     }
 }
--- test/CheckersTest.java
+++ test/CheckersTest.java
@@ -1,7 +1,14 @@
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.*;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class CheckersTest {
 
     @Nested
@@ -41,25 +48,143 @@ public class CheckersTest {
     class getLegalMoves {
 
         @Test
-        public void should_get_empty_list_for_piece_without_legal_moves() {
+        public void should_get_empty_list_for_non_board_square() {
             Checkers checkers = new Checkers();
             checkers.setup();
 
-            List<Move> legalMoves = checkers.getLegalMoves(0);
+            List<Move> legalMoves = checkers.getLegalMoves(100);
 
             assertEquals(0, legalMoves.size());
         }
 
         @Test
-        public void should_get_legal_moves_for_some_pieces() {
+        public void should_get_empty_list_for_piece_without_legal_moves() {
             Checkers checkers = new Checkers();
             checkers.setup();
 
-            List<Move> legalMoves = checkers.getLegalMoves(8);
+            List<Move> legalMoves = checkers.getLegalMoves(1);
 
-            assertEquals(1, legalMoves.size());
-            assertEquals(8, legalMoves.get(0).origin());
-            assertEquals(12, legalMoves.get(0).target());
+            assertEquals(0, legalMoves.size());
+        }
+        
+        @Nested
+        @DisplayName("when on an odd row")
+        class OddRow {
+            Checkers checkers;
+
+            @BeforeEach
+            void setUp() {
+                int[] board = new int[]{0,0,0,0, 1,0,0,1, 0,0,0,0};
+                checkers = new Checkers(board);
+            }
+
+            @Test
+            void should_get_one_legal_move_for_left_edge_piece() {
+                List<Move> legalMoves = checkers.getLegalMoves(5);
+                assertEquals(legalMoves.size(), 1);
+                assertThat(legalMoves, hasItem(matchingMove(5, 9)));
+            }
+
+            @Test
+            void should_get_two_legal_moves_for_right_edge_piece() {
+                List<Move> legalMoves = checkers.getLegalMoves(8);
+                assertEquals(legalMoves.size(), 2);
+                assertThat(legalMoves, hasItem(matchingMove(8, 11)));
+                assertThat(legalMoves, hasItem(matchingMove(8, 12)));
+            }
+        }
+
+        @Nested
+        @DisplayName("when on an even row")
+        class EvenRow {
+            Checkers checkers;
+
+            @BeforeEach
+            void setUp() {
+                int[] board = new int[]{1,0,0,1, 0,0,0,0, 0,0,0,0};
+                checkers = new Checkers(board);
+            }
+
+            @Test
+            void should_get_two_legal_moves_for_left_edge_piece() {
+                List<Move> legalMoves = checkers.getLegalMoves(1);
+
+                assertEquals(2, legalMoves.size());
+                assertThat(legalMoves, hasItem(matchingMove(1, 5)));
+                assertThat(legalMoves, hasItem(matchingMove(1, 6)));
+            }
+
+            @Test
+            public void should_get_one_legal_move_for_right_edge_piece() {
+                List<Move> legalMoves = checkers.getLegalMoves(4);
+
+                assertEquals(1, legalMoves.size());
+                assertThat(legalMoves, hasItem(matchingMove(4, 8)));
+            }
         }
     }
-}
+
+    @Nested
+    @DisplayName("getAllLegalMoves")
+    class getAllLegalMoves {
+        private Checkers checkers;
+
+        @BeforeEach
+        void setUp() {
+            checkers = new Checkers();
+            checkers.setup();
+        }
+
+        @Test
+        void should_get_all_player_one_legal_moves() {
+            List<Move> moves = checkers.getAllLegalMoves(1);
+
+            assertEquals(moves.size(), 7);
+            assertThat(moves, hasItem(matchingMove(9, 13)));
+            assertThat(moves, hasItem(matchingMove(9, 14)));
+            assertThat(moves, hasItem(matchingMove(10, 14)));
+            assertThat(moves, hasItem(matchingMove(10, 15)));
+            assertThat(moves, hasItem(matchingMove(11, 15)));
+            assertThat(moves, hasItem(matchingMove(11, 16)));
+            assertThat(moves, hasItem(matchingMove(12, 16)));
+        }
+
+        @Test
+        void should_get_all_player_two_legal_moves() {
+            List<Move> moves = checkers.getAllLegalMoves(2);
+
+            assertEquals(moves.size(), 7);
+            assertThat(moves, hasItem(matchingMove(21, 17)));
+            assertThat(moves, hasItem(matchingMove(22, 17)));
+            assertThat(moves, hasItem(matchingMove(22, 18)));
+            assertThat(moves, hasItem(matchingMove(23, 18)));
+            assertThat(moves, hasItem(matchingMove(23, 19)));
+            assertThat(moves, hasItem(matchingMove(24, 19)));
+            assertThat(moves, hasItem(matchingMove(24, 20)));
+        }
+    }
+
+
+
+    private Matcher<Move> matchingMove(int origin, int target) {
+        return new TypeSafeMatcher<Move>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("move");
+            }
+
+            public boolean matchesSafely(Move actual) {
+                return actual.target() == target
+                    && actual.origin() == origin;
+            }
+
+            public void describeMismatchSafely(Move move, Description mismatchDescription) {
+                mismatchDescription.appendText("Expected [" + origin + "->" + target + "]"
+                        + " but got [" + move.origin() + "->" + move.target() + "]");
+            }
+        };
+
+    }
+
+};
